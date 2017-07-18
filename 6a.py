ad906ebcb8a84428dd33d2e0ee7b696b78fa6a34
Nadine <nadine.said@etu.univ-amu.fr>
2017-07-18 23:41:59 +0200

    maj

--- 6a.py
+++ 6a.py
@@ -7,6 +7,22 @@ def remplissageTableau ():
 	for i in range(7):
 		tab.append(0)
 	print(tab)
-remplissageTableau ()	
-		
-		
+#remplissageTableau ()
+
+"""	
+Ecrire un algorithme qui déclare et remplisse un tableau contenant les six voyelles de l’alphabet latin.		
+"""		
+#JE SAIS PAS
+	
+"""
+Ecrire un algorithme qui déclare un tableau de 9 notes, dont on fait ensuite saisir les valeurs par l’utilisateur.
+"""
+
+def tableNote():
+	note = []
+	for i in range(9):
+		nb = int(input("Entrez les notes : "))
+		note.append(nb)
+	#print(note)
+	print(note[2])
+tableNote()
