7f319b8816e21f26c7f298b412425756012ef952
Nadine <nadine.said@etu.univ-amu.fr>
2017-07-18 22:56:05 +0200

    exo progra

---
+++ 6a.py
@@ -0,0 +1,12 @@
+"""
+Ecrire un algorithme qui déclare et remplisse un tableau de 7 valeurs numériques en les mettant toutes à zéro.
+"""
+
+def remplissageTableau ():
+	tab=[]
+	for i in range(7):
+		tab.append(0)
+	print(tab)
+remplissageTableau ()	
+		
+		
